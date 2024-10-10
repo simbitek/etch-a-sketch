e0c9cb4155ab3d14ac3b32778efd78833e33b851
Simbitek <wambuiweruh@gmail.com>
2024-10-10 23:50:26 +0300

    Create 16 by 16 grid

--- sketch.js
+++ sketch.js
@@ -1,47 +1,40 @@
-// const container = document.getElementById('#container');
-// const gridSizeBtn = document.getElementById('#grid-size-btn');
+const divContainer = document.getElementById('container');
+const gridSizeBtn = document.querySelector('#grid-size-btn');
+const inputText = document.querySelector('#input-text');
 
-// function createGrid(size) {
-//     container.innerHTML = '';
-//     container.style.gridTemplateColumns = `repeat(${16}, 1fr)`;
-//     container.style.gridTemplateRows = `repeat(${16}, 1fr)`;
+function createGrid() {
+    const divGrid = document.createElement('div');
+    divGrid.style.height = '16px';
+    divGrid.style.width = '16px';
+    // divGrid.style.backgroundColor ='blue';
+    divGrid.classList.add('square');
+    divContainer.appendChild(divGrid);
 
-//     for (let i = 0; i < 16 * 16; i++) {
-//         let square = document.createElement('div');
-//         square.classList.add('grid-square');
-//         square.style.width  = `$(256 / 16)px`;
-//         square.style.height = `$(256 / 16)px`;
-
-//         // hover effect
-
-//         square.addEventListener('mouseover', () => {
-//             square.style.backgroundColor = 'red';
-//         });
-
-//         container.appendChild(square);
-//     }
-
-// }
-
-// gridSizeBtn.addEventListener('click', () => {
-//     let gridSize = alert("Enter grid size (max 100): ");
-//     if (gridSize > 100) gridSize = 100;
-//     if (gridSize < 1) gridSize = 1;
-//     createGrid(gridSize);
-// });
-
-// createGrid(16);
- 
-
-const container = document.querySelector('#container');
-container.style.gridTemplateColumns = `repeat(16, 1fr)`;
-container.style.gridTemplateRows = `repeat(16, 1fr)`;
-
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.style.backgroundColor ='blue';
-    container.insertAdjacentElement('beforeend', square);
 }
+
+
+gridSizeBtn.addEventListener('click', () => {
+    while (divContainer.firstChild) {
+        divContainer.removeChild(divContainer.lastChild);
+      }
+    let gridSize = inputText.value.toString();
+    console.log('grid size: ' +gridSize)
+    if (gridSize > 100) gridSize = 100;
+    if (gridSize < 1) gridSize = 1;
+
+    for (let index = 0; index < gridSize; index++) {
+        
+        createGrid();
+    }
+    
+
+
+});
+
+
+
+
+
 
 
 
